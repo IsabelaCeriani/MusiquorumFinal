46e8a3058889971a987a1a0cfd3e0ebf02997090
Isabela Ceriani <isabela.ceriani@ing.austral.edu.ar>
2021-11-29 10:18:28 -0300

    Fixed bug

--- Desktop/musiquorumSpring/src/main/java/com/lab1Spring/musiquorum/controllers/FileController.java
+++ Desktop/musiquorumSpring/src/main/java/com/lab1Spring/musiquorum/controllers/FileController.java
@@ -1,62 +1,61 @@
-package com.lab1Spring.musiquorum.controllers;
-
-import com.lab1Spring.musiquorum.models.ClassFile;
-import com.lab1Spring.musiquorum.responses.UploadFileResponse;
-import com.lab1Spring.musiquorum.services.FileService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
-
-@RestController
-public class FileController {
-
-    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
-
-    @Autowired
-    private FileService dbFileStorageService;
-
-    @PostMapping("/uploadFile")
-    public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) {
-        ClassFile dbClassFile = dbFileStorageService.storeFile(file);
-
-        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path("/downloadFile/")
-                .path(dbClassFile.getId().toString())
-                .toUriString();
-
-        return new UploadFileResponse(dbClassFile.getFileName(), fileDownloadUri,
-                file.getContentType(), file.getSize());
-    }
-
-    @PostMapping("/uploadMultipleFiles")
-    public List<UploadFileResponse> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
-        return Arrays.asList(files)
-                .stream()
-                .map(file -> uploadFile(file))
-                .collect(Collectors.toList());
-    }
-
-    @GetMapping("/downloadFile/{fileId}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable UUID fileId) {
-        // Load file from database
-        ClassFile dbClassFile = dbFileStorageService.getFile(fileId);
-
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(dbClassFile.getFileType()))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + dbClassFile.getFileName() + "\"")
-                .body(new ByteArrayResource(dbClassFile.getData()));
-    }
-}
+//package com.lab1Spring.musiquorum.controllers;
+//
+//import com.lab1Spring.musiquorum.models.ClassFile;
+//import com.lab1Spring.musiquorum.responses.UploadFileResponse;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.core.io.ByteArrayResource;
+//import org.springframework.core.io.Resource;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.MediaType;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.multipart.MultipartFile;
+//import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+//
+//import java.util.Arrays;
+//import java.util.List;
+//import java.util.UUID;
+//import java.util.stream.Collectors;
+//
+//@RestController
+//public class FileController {
+//
+//    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+//
+//    @Autowired
+//    private FileService dbFileStorageService;
+//
+//    @PostMapping("/uploadFile")
+//    public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) {
+//        ClassFile dbClassFile = dbFileStorageService.storeFile(file);
+//
+//        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+//                .path("/downloadFile/")
+//                .path(dbClassFile.getId().toString())
+//                .toUriString();
+//
+//        return new UploadFileResponse(dbClassFile.getFileName(), fileDownloadUri,
+//                file.getContentType(), file.getSize());
+//    }
+//
+//    @PostMapping("/uploadMultipleFiles")
+//    public List<UploadFileResponse> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+//        return Arrays.asList(files)
+//                .stream()
+//                .map(file -> uploadFile(file))
+//                .collect(Collectors.toList());
+//    }
+//
+//    @GetMapping("/downloadFile/{fileId}")
+//    public ResponseEntity<Resource> downloadFile(@PathVariable UUID fileId) {
+//        // Load file from database
+//        ClassFile dbClassFile = dbFileStorageService.getFile(fileId);
+//
+//        return ResponseEntity.ok()
+//                .contentType(MediaType.parseMediaType(dbClassFile.getFileType()))
+//                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + dbClassFile.getFileName() + "\"")
+//                .body(new ByteArrayResource(dbClassFile.getData()));
+//    }
+//}
--- Desktop/musiquorumSpring/src/main/java/com/lab1Spring/musiquorum/services/FileService.java
+++ Desktop/musiquorumSpring/src/main/java/com/lab1Spring/musiquorum/services/FileService.java
@@ -1,45 +1,45 @@
-package com.lab1Spring.musiquorum.services;
-
-import com.lab1Spring.musiquorum.exceptions.BadRequestException;
-import com.lab1Spring.musiquorum.exceptions.FileStorageException;
-import com.lab1Spring.musiquorum.models.ClassFile;
-import com.lab1Spring.musiquorum.repositories.FileRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.Date;
-import java.util.Objects;
-import java.util.UUID;
-
-@Service
-public class FileService {
-
-    @Autowired
-    private FileRepository dbFileRepository;
-
-    public ClassFile storeFile(MultipartFile file) {
-        // Normalize file name
-        String fileName = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
-
-        try {
-            // Check if the file's name contains invalid characters
-            if(fileName.contains("..")) {
-                throw new FileStorageException("Sorry! Filename contains invalid path sequence " + fileName);
-            }
-
-            ClassFile dbClassFile = new ClassFile(fileName, file.getContentType(), new Date(), file.getBytes());
-
-            return dbFileRepository.save(dbClassFile);
-        } catch (IOException | FileStorageException ex) {
-            throw new FileStorageException("Could not store file " + fileName + ". Please try again!");
-        }
-    }
-
-    public ClassFile getFile(UUID fileId) {
-        return dbFileRepository.findById(fileId)
-                .orElseThrow(() -> new BadRequestException("File not found with id " + fileId));
-    }
-}
+//package com.lab1Spring.musiquorum.services;
+//
+//import com.lab1Spring.musiquorum.exceptions.BadRequestException;
+//import com.lab1Spring.musiquorum.exceptions.FileStorageException;
+//import com.lab1Spring.musiquorum.models.ClassFile;
+//import com.lab1Spring.musiquorum.repositories.FileRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import org.springframework.util.StringUtils;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import java.io.IOException;
+//import java.util.Date;
+//import java.util.Objects;
+//import java.util.UUID;
+//
+//@Service
+//public class FileService {
+//
+//    @Autowired
+//    private FileRepository dbFileRepository;
+//
+//    public ClassFile storeFile(MultipartFile file) {
+//        // Normalize file name
+//        String fileName = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+//
+//        try {
+//            // Check if the file's name contains invalid characters
+//            if(fileName.contains("..")) {
+//                throw new FileStorageException("Sorry! Filename contains invalid path sequence " + fileName);
+//            }
+//
+//            ClassFile dbClassFile = new ClassFile(fileName, file.getContentType(), new Date(), file.getBytes());
+//
+//            return dbFileRepository.save(dbClassFile);
+//        } catch (IOException | FileStorageException ex) {
+//            throw new FileStorageException("Could not store file " + fileName + ". Please try again!");
+//        }
+//    }
+//
+//    public ClassFile getFile(UUID fileId) {
+//        return dbFileRepository.findById(fileId)
+//                .orElseThrow(() -> new BadRequestException("File not found with id " + fileId));
+//    }
+//}
